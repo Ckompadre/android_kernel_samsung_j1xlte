0a1dbc45599279bb304fc43975ae35f9cbab41df
Ckompadre <Bezrukovila200212@gmail.com>
2021-06-18 21:13:01 +0500

    f_acm.c: Fixed function

--- drivers/usb/gadget/f_acm.c
+++ drivers/usb/gadget/f_acm.c
@@ -541,7 +541,8 @@ static int acm_notify_serial_state(struct f_acm *acm)
 	struct usb_composite_dev *cdev = acm->port.func.config->cdev;
 	int			status;
 	unsigned long	flags;
-
+	__le16			serial_state;
+	
 	spin_lock_irqsave(&acm->lock, flags);
 
 	if (acm->notify_req) {
